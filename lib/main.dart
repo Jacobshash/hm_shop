bbb04edd12619481d545b448761af8780cd97c84
loltoulan <lol_toulan@163.com>
2025-12-11 19:21:24 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HmShop());
+}
+
+class HmShop extends StatelessWidget {
+  const HmShop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
